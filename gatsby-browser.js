864a9d4e4cfa9d077ddea3cc930e4d4bdb151fc9
stefan0982 <stefan.c.0608@mail.ru>
2021-02-24 13:45:23 +0200

    pwa removed auto refresh

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -21,15 +21,9 @@
 
 // exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
 
-export const onRouteUpdate = () => {
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
-    reg.update();
-  });
-};
 
 export const onServiceWorkerUpdateReady = () => {
   // Set window.___swUpdated to prevent update on page navigation.
   // Overrides https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby/cache-dir/navigation.js#L64
   window.___swUpdated = false
-  window.location.reload()
 }
--- src/PwaButton.js
+++ src/PwaButton.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useSWUpdateChecker } from "service-worker-updater"
-const CHECK_INTERVAL = 10 * 60 // 10 minute = 10 * 60 * 1000
-const UPDATE_ON_LOAD = true
+const CHECK_INTERVAL = 10 * 60 * 1000 // 10 minute = 10 * 60 * 1000
+const UPDATE_ON_LOAD = false
 export default function UpdateButton() {
   const [hasUpdate, updateHandler] = useSWUpdateChecker({
     checkInterval: CHECK_INTERVAL,
