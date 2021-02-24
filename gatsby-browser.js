6b3b3606aedd466f9c2375876f9e7bb2b1c9eab8
stefan0982 <stefan.c.0608@mail.ru>
2021-02-24 10:15:26 +0200

    pwa refresh fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
-export const onRouteUpdate = () => {
-  navigator.serviceWorker.register('/sw.js').then((reg) => {
-    reg.update();
-  });
-};
+// export const onRouteUpdate = () => {
+//   navigator.serviceWorker.register('/sw.js').then((reg) => {
+//     reg.update();
+//   });
+// };
 
 // export const onServiceWorkerUpdateReady = () => {
 //   window.location.reload(true)
