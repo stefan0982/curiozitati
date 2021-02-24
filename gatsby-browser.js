e7be2f6878ba89fff9dce7c09eb7057ef538a5ff
stefan0982 <stefan.c.0608@mail.ru>
2021-02-24 10:10:40 +0200

    pwa refresh try

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,22 @@
-// export const onRouteUpdate = () => {
-//   navigator.serviceWorker.register('/sw.js').then((reg) => {
-//     reg.update();
-//   });
-// };
+export const onRouteUpdate = () => {
+  navigator.serviceWorker.register('/sw.js').then((reg) => {
+    reg.update();
+  });
+};
 
 // export const onServiceWorkerUpdateReady = () => {
 //   window.location.reload(true)
 // };
 
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `Această aplicație a fost actualizată ` +
-    `Reâncarcă pentru a vedea schimbările`
-  )
+// export const onServiceWorkerUpdateReady = () => {
+//   const answer = window.confirm(
+//     `Această aplicație a fost actualizată ` +
+//     `Reâncarcă pentru a vedea schimbările`
+//   )
+//
+//   if (answer === true) {
+//     window.location.reload()
+//   }
+// }
 
-  if (answer === true) {
-    window.location.reload()
-  }
-}
+exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
